5fe773c68041662a2b35725b708befbf25ebad4b
Coremotor <coremotor@users.noreply.github.com>
2020-08-30 17:57:07 +0300

    inDev

--- src/Header/JobSought/JobSought.js
+++ src/Header/JobSought/JobSought.js
@@ -7,6 +7,11 @@ const JobSought = () => {
             <h1 className={styles.jobSoughtTitle}>Соискаемая должность</h1>
             <p className={styles.jobSoughtText}>Front-end developer</p>
             <p className={styles.jobSoughtText}>Демаков Андрей Иванович</p>
+            <p>
+                <a className={styles.jobSoughtLink} href="https://github.com/Coremotor"
+                   target="_blank"
+                   rel="noopener noreferrer">Github repositories</a>
+            </p>
         </section>
     )
 }
--- src/Header/Photo/Photo.js
+++ src/Header/Photo/Photo.js
@@ -4,9 +4,9 @@ import styles from './Photo.module.css';
 const Photo = () => {
 
     const photoLinkArr = [
-        'https://media.istockphoto.com/photos/eyes-full-of-curiosity-picture-id1187105189?b=1&k=6&m=1187105189&s=170667a&w=0&h=l9jf9X5LFSylwiRnxUEfD9qaqnHoTGSYyEQqY08IojY=',
-        'https://media.istockphoto.com/photos/gray-cat-on-the-floor-of-the-room-near-a-bowl-of-water-view-from-picture-id1191739263?b=1&k=6&m=1191739263&s=170667a&w=0&h=UmwR25J6R8ZT5oHXln60zonGklHjzZCpsZQ2v4oPh9w=',
-        'https://media.istockphoto.com/photos/ginger-cat-sleepng-on-couch-in-living-room-surrounded-with-cushions-picture-id1153834825?b=1&k=6&m=1153834825&s=170667a&w=0&h=qz32jz8Xu2Qv0Csp4Ynb0HNvxj6xywqJ8ADV_0csxrU=',
+        'http://coremotor.ru/img/1.jpg',
+        'http://coremotor.ru/img/2.jpg',
+        'http://coremotor.ru/img/3.jpg',
     ]
 
     const [count, setCount] = useState(0);
