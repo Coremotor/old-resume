b9bb31e6deb04e562f5826ede81d5c5f4ba8599f
Coremotor <coremotor@users.noreply.github.com>
2020-08-31 14:45:14 +0300

    inDev

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Fragment} from 'react';
-// import styles from './App.module.css';
+import styles from './App.module.css';
 import CoursesList from "./CoursesList/CoursesList";
 import Header from "./Header/Header";
 import WhatCanISuggest from "./WhatCanISuggest/WhatCanISuggest";
@@ -14,6 +14,10 @@ function App() {
 
     return (
         <Fragment>
+            <div className={styles.textWrapper}>
+                <small className={styles.text}>App create on React Redux</small>
+                <small className={styles.text}>version 0.1.0-&alpha;</small>
+            </div>
             <Header/>
             <WhatCanISuggest/>
             <Stack/>
